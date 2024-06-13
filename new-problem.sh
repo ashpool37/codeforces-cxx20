f1e9fd5ffd6a127b783087e21493aa369d727d1e
Ashpool <ashpool@xecut.net>
2024-06-13 19:32:04 +0200

    feat: add launch.json template

--- new-problem.sh
+++ new-problem.sh
@@ -2,7 +2,10 @@
 
 problem="$1"
 mkdir "${problem}"
+mkdir "${problem}/.vscode"
 sed "s/%PROBLEM%/${problem}/g" Makefile.template \
     > "${problem}/Makefile"
+sed "s/%PROBLEM%/${problem}/g" launch.json.template \
+    > "${problem}/.vscode/launch.json"
 cp .clangd "${problem}/.clangd"
 cp main.cpp "${problem}/main.cpp"
