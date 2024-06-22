4264377fb03213b98399c8b11bdc78d71b5055aa
Ashpool <ashpool@xecut.net>
2024-06-22 21:04:45 +0200

    feat: store all templates in a separate directory

--- new-problem.sh
+++ new-problem.sh
@@ -3,9 +3,9 @@
 problem="$1"
 mkdir "${problem}"
 mkdir "${problem}/.vscode"
-sed "s/%PROBLEM%/${problem}/g" Makefile.template \
+sed "s/%PROBLEM%/${problem}/g" .templates/Makefile.template \
     > "${problem}/Makefile"
-sed "s/%PROBLEM%/${problem}/g" launch.json.template \
+sed "s/%PROBLEM%/${problem}/g" .templates/launch.json.template \
     > "${problem}/.vscode/launch.json"
-cp .clangd "${problem}/.clangd"
-cp main.cpp "${problem}/main.cpp"
+cp .templates/.clangd "${problem}/.clangd"
+cp .templates/main.cpp "${problem}/main.cpp"
