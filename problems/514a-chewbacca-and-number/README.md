bfdba7e6e9834ea530538a97d6a17824e4c37c4c
Ashpool <ashpool@xecut.net>
2024-07-20 00:35:49 +0200

    solve: 514a chewbacca and number

---
+++ problems/514a-chewbacca-and-number/README.md
@@ -0,0 +1,3 @@
+# 514A. Chewbacca and Number
+
+[Problem statement on Codeforces](https://codeforces.com/problemset/problem/514/A?locale=en).
--- problems/514a-chewbacca-and-number/main.cpp
+++ problems/514a-chewbacca-and-number/main.cpp
@@ -1,13 +1,16 @@
-// Problem: {{ contest_number }}{{ problem_letter_upper }}. {{ problem_title }}
-// Problem statement: https://codeforces.com/problemset/problem/{{ contest_number }}/{{ problem_letter_upper }}?locale=en
+// Problem: 514A. Chewbacca and Number
+// Problem statement: https://codeforces.com/problemset/problem/514/A?locale=en
 // Solution author: Artem Zhurikhin (https://codeforces.com/profile/Ashpool)
 // Solution license: the Unlicense (Public Domain)
 // More solutions: https://github.com/ashpool37/codeforces-cxx20
 
+#include <cmath>
 #include <iostream>
 #include <charconv>
 #include <ranges>
-#include <type_traits>
+#include <sstream>
+#include <cstdint>
+#include <cmath>
 
 /* #region templates */
 
@@ -42,10 +45,28 @@ auto power(BaseT base, ExponentT exponent) {
 
 /* #endregion */
 
+unsigned chewbacca_get_digit(std::istream& input_stream) {
+    char digit;
+    input_stream >> digit;
+    unsigned digit_value = digit_to_unsigned(digit);
+    digit_value = std::min(digit_value, 9 - digit_value);
+    return digit_value;
+}
+
 int main() {
-    unsigned test_count;
-    std::cin >> test_count;
-    while(test_count--) {
-        
+    std::string number_string;
+    std::getline(std::cin, number_string);
+
+    std::istringstream number_istream(number_string);
+    umax result = 0;
+    umax digit_value;
+    bool got_significant_digit = false;
+    for(umax multiplier : range(static_cast<int>(number_string.size() - 1), -1, number_string.size())) {
+        digit_value = chewbacca_get_digit(number_istream);
+        if(digit_value == 0 and not got_significant_digit)
+            digit_value = 9;
+        result += digit_value * power(10ull, multiplier);
+        got_significant_digit = true;
     }
+    std::cout << result << std::endl;
 }
