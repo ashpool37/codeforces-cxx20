ffba5a79d36257a4486f3033f4e71b920763983e
Ashpool <ashpool@xecut.net>
2024-07-25 01:20:09 +0200

    solve: 363b fence

---
+++ problems/363b-fence/README.md
@@ -0,0 +1,3 @@
+# 363B. Fence
+
+[Problem statement on Codeforces](https://codeforces.com/problemset/problem/363/B?locale=en).
---
+++ problems/363b-fence/main.cpp
@@ -0,0 +1,197 @@
+// Problem: 363B. Fence
+// Problem statement: https://codeforces.com/problemset/problem/363/B?locale=en
+// Solution author: Artem Zhurikhin (https://codeforces.com/profile/Ashpool)
+// Solution license: the Unlicense (Public Domain)
+// More solutions: https://github.com/ashpool37/codeforces-cxx20
+
+#include <iostream>
+#include <charconv>
+#include <ranges>
+#include <type_traits>
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <array>
+#include <limits>
+#include <iterator>
+#include <unordered_set>
+#include <queue>
+
+/* #region templates */
+
+using umax = std::uintmax_t;
+
+unsigned digit_to_unsigned(char c) {
+    char cs[2] = "\0";
+    cs[0] = c;
+    unsigned result;
+    std::from_chars(cs, cs+1, result);
+    return result;
+}
+
+char unsigned_to_digit(unsigned n) {
+    char cs[2] = "\0";
+    std::to_chars(cs, cs+1, n);
+    return cs[0];
+}
+
+template<std::integral T>
+auto counted(T start, size_t count, T step = 1) {
+    using namespace std::ranges::views;
+    return iota(0u, count) | transform([=](size_t i){ return start + step * static_cast<T>(i); });
+}
+
+auto counted(size_t count) {
+    return counted(0ull, count, 1ull);
+}
+
+template<std::integral BaseT, std::integral ExponentT>
+auto power(BaseT base, ExponentT exponent) {
+    std::common_type_t<BaseT, ExponentT> result = 1;
+    while(exponent--) result *= base;
+    return result;
+}
+
+template <typename T>
+concept ReadableFromStream = requires(std::istream is, T value) {
+    { is >> value };
+};
+
+class FromIstreamHelper {
+    std::istream& input_stream;
+    std::optional<std::size_t> count;
+
+    FromIstreamHelper(FromIstreamHelper const&) = delete;
+    FromIstreamHelper(FromIstreamHelper&&) = delete;
+    FromIstreamHelper& operator=(FromIstreamHelper const&) = delete;
+    FromIstreamHelper& operator=(FromIstreamHelper&&) = delete;
+
+    FromIstreamHelper(std::istream& input_stream, std::size_t count)
+        : input_stream(input_stream), count(count)
+    {}
+    FromIstreamHelper(std::istream& input_stream)
+        : input_stream(input_stream), count(std::nullopt)
+    {}
+
+    friend auto from_istream(std::istream& input_stream);
+    friend auto from_istream(std::istream& input_stream, std::size_t count);
+public:
+
+    operator std::string() && {
+        std::string result;
+        std::getline(input_stream, result);
+        return result;
+    }
+
+    template<ReadableFromStream T>
+    operator T() && {
+        T result;
+        input_stream >> result;
+        return result;
+    }
+
+    template<ReadableFromStream T>
+    operator std::vector<T>() && {
+        std::vector<T> result;
+        if(count) {
+            result.reserve(*count);
+            std::copy_n(std::istream_iterator<T>(input_stream), *count, std::back_inserter(result));
+        }
+        return result;
+    }
+
+    template<ReadableFromStream T>
+    operator std::list<T>() && {
+        std::list<T> result;
+        if(count) {
+            std::copy_n(std::istream_iterator<T>(input_stream), *count, std::back_inserter(result));
+        }
+        return result;
+    }
+
+    template<ReadableFromStream T, std::size_t array_size>
+    operator std::array<T, array_size>() && {
+        std::array<T, array_size> result;
+        std::copy_n(std::istream_iterator<T>(input_stream), array_size, std::begin(result));
+        return result;
+    }
+};
+
+auto from_istream(std::istream& input_stream) {
+    return FromIstreamHelper(input_stream);
+}
+
+auto from_istream(std::istream& input_stream, std::size_t count) {
+    return FromIstreamHelper(input_stream, count);
+}
+
+auto from_cin() {
+    return from_istream(std::cin);
+}
+
+auto from_cin(std::size_t count) {
+    return from_istream(std::cin, count);
+}
+
+void istream_skip_line(std::istream& input_stream) {
+    input_stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void cin_skip_line() {
+    istream_skip_line(std::cin);
+}
+
+
+template<std::integral T, std::output_iterator<T> OutputIt>
+void generate_multiples(T limit, T base, OutputIt it) {
+    T multiple;
+    for(T i = 2; (multiple = base * i) <= limit; i++) {
+        *it = multiple;
+        it++;
+    }
+}
+
+template<std::integral T, std::output_iterator<T> OutputIt>
+void generate_primes(T limit, OutputIt it) {
+    std::unordered_set<T> composite;
+    for(T i = 2; i <= limit; i++) {
+        if(not composite.contains(i)) {
+            *it = i;
+            it++;
+            if(i * i <= limit) generate_multiples(limit, i, std::inserter(composite, composite.begin()));
+        }
+    }
+}
+
+/* #endregion */
+
+int main() {
+    unsigned fence_length = from_cin();
+    unsigned piano_width = from_cin();
+
+    unsigned total_broken_height = 0;
+    std::queue<unsigned> planks_broken;
+    for([[maybe_unused]] auto _ : counted(piano_width)) {
+        unsigned plank = from_cin();
+        total_broken_height += plank;
+        planks_broken.push(plank);
+    }
+
+    unsigned total_broken_height_min = total_broken_height;
+    unsigned hole_position_min = 0;
+    for(unsigned hole_position : counted(1u, fence_length - piano_width)) {
+        unsigned plank = from_cin();
+        total_broken_height += plank;
+        planks_broken.push(plank);
+        total_broken_height -= planks_broken.front();
+        planks_broken.pop();
+
+        if(total_broken_height_min > total_broken_height) {
+            hole_position_min = hole_position;
+            total_broken_height_min = total_broken_height;
+        }
+    }
+
+    std::cout << hole_position_min + 1 << std::endl;
+}
